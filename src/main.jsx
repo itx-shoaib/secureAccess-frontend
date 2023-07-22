700eb2f949c0ece74419cb742b2eeedbcda5b291
itx-shoaib <shoaibjamil43@gmail.com>
2023-07-23 00:58:20 +0500

    Adding Provider

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,11 @@ import {
 } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css'
+import {Provider} from "react-redux"
 import HomeScreen from './screens/HomeScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
+import store from "./store"
 
 
 const router = createBrowserRouter(
@@ -28,7 +30,9 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <React.StrictMode>
   <RouterProvider router={router} />
-</React.StrictMode>,
+  </React.StrictMode>,
+  </Provider>
 )
