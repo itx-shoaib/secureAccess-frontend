db414d6a24c8caa9b6d7f5a3f706f38f192793de
itx-shoaib <shoaibjamil43@gmail.com>
2023-07-23 01:11:56 +0500

    Making the authSlice

--- src/store.jsx
+++ src/store.jsx
@@ -1,7 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice"
 
 const store = configureStore({
-reducer : {},
+reducer : {
+    auth:authReducer
+},
 middleware:(getDefaultMiddleware)=>getDefaultMiddleware(), //Middleware is a way to extend Redux with custom functionality
 devTools:true
 })
