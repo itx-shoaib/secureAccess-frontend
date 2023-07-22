40d977c62b278060de2cab7d8309300142b3059d
itx-shoaib <shoaibjamil43@gmail.com>
2023-07-23 03:41:42 +0500

    Integrating update profile api

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
 import { toast } from 'react-toastify';
 import Loader from '../components/Loader';
+import { useUpdateUserMutation } from '../slices/userApiSlice';
+import { setCredentials } from '../slices/authSlice';
 
 const ProfileScreen = () => {
   const [email, setEmail] = useState('');
@@ -12,10 +14,38 @@ const ProfileScreen = () => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
-  const submitHandler = async (e) => {
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+
+const [updateProfile, { isLoading }] = useUpdateUserMutation();
+
+useEffect(() => {
+  setName(userInfo.name);
+  setEmail(userInfo.email);
+}, [userInfo.email, userInfo.name]);
+
+const submitHandler = async (e) => {
     e.preventDefault();
-    console.log('submit');
+    if (password !== confirmPassword) {
+      toast.error('Passwords do not match');
+    } else {
+      try {
+        const res = await updateProfile({
+          _id: userInfo._id,
+          name,
+          email,
+          password,
+        }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        toast.success('Profile updated successfully');
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
   };
+
+  if(isLoading) return <Loader/>
 
   return (
     <FormContainer>
--- src/slices/userApiSlice.js
+++ src/slices/userApiSlice.js
@@ -22,8 +22,15 @@ export const userApiSlice = apiSlice.injectEndpoints({
                 method :'POST',
                 body: data
             })
-        })
+        }),
+        updateUser: builder.mutation({
+            query: (data) => ({
+              url: `${USERS_URL}/profile`,
+              method: 'PUT',
+              body: data,
+            }),
+          }),
     })
 })
 
-export const {useLoginMutation, useLogoutMutation, useRegisterMutation} = userApiSlice
+export const {useLoginMutation, useLogoutMutation, useRegisterMutation, useUpdateUserMutation,} = userApiSlice
