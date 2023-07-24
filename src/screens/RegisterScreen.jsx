475fed24d519f91b3c1242ce5e3e6bdf7710f3bb
itx-shoaib <shoaibjamil43@gmail.com>
2023-07-25 01:13:02 +0500

    Adding formik in register screen

--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -1,18 +1,25 @@
-import { useState, useEffect } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { Link,  useNavigate } from "react-router-dom";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 import { useDispatch, useSelector } from "react-redux";
 import { useRegisterMutation } from "../slices/userApiSlice";
 import { setCredentials } from "../slices/authSlice";
 import { toast } from "react-toastify";
+import { useFormik } from "formik";
+import * as yup from "yup";
 import Loader from "../components/Loader";
 
+const schema = yup.object().shape({
+  email: yup.string()
+  .email("Provide a valid email address")
+  .required("Email is required"),
+password: yup.string().required("Password is required"),
+confirmPassword: yup.string().required("Confirm Password is required"),
+name: yup.string().required("Name is required"),
+});
+
 const RegisterScreen = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -27,34 +34,42 @@ const RegisterScreen = () => {
     }
   }, [navigate, userInfo]);
 
-  const submitHandler = async (e) => {
-    e.preventDefault();
-
-    if (password !== confirmPassword) {
-      toast.error("Passwords do not match");
-    } else {
-      try {
-        const res = await register({ name, email, password }).unwrap();
-        dispatch(setCredentials({ ...res }));
-        navigate("/");
-      } catch (err) {
-        toast.error(err?.data?.message || err.error);
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      name:'',
+      password: '',
+      confirmPassword: ''
+    },
+    validationSchema: schema,
+    onSubmit: async (values) => {
+      if (values.password !== values.confirmPassword) {
+        toast.error("Passwords do not match");
+      } else {
+        try {
+          const res = await register({ name:values.name, email: values.email, password: values.password }).unwrap();
+          dispatch(setCredentials({ ...res }));
+          navigate("/");
+        } catch (err) {
+          toast.error(err?.data?.message || err.error);
+        }
       }
-    }
-  };
+    },
+  });
 
   return (
     <FormContainer>
       <h1>Register</h1>
-      <Form onSubmit={submitHandler}>
+      <Form onSubmit={formik.handleSubmit}>
         <Form.Group className="my-2" controlId="name">
           <Form.Label>Name</Form.Label>
           <Form.Control
             type="name"
             placeholder="Enter name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={formik.values.name}
+            {...formik.getFieldProps('name')}
           ></Form.Control>
+          <small>{formik.errors.name}</small>
         </Form.Group>
 
         <Form.Group className="my-2" controlId="email">
@@ -62,9 +77,10 @@ const RegisterScreen = () => {
           <Form.Control
             type="email"
             placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={formik.values.email}
+            {...formik.getFieldProps('email')}
           ></Form.Control>
+          <small>{formik.errors.email}</small>
         </Form.Group>
 
         <Form.Group className="my-2" controlId="password">
@@ -72,21 +88,23 @@ const RegisterScreen = () => {
           <Form.Control
             type="password"
             placeholder="Enter password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={formik.values.password}
+            {...formik.getFieldProps('password')}
           ></Form.Control>
+          <small>{formik.errors.password}</small>
         </Form.Group>
         <Form.Group className="my-2" controlId="confirmPassword">
           <Form.Label>Confirm Password</Form.Label>
           <Form.Control
             type="password"
             placeholder="Confirm password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
+            value={formik.values.confirmPassword}
+            {...formik.getFieldProps('confirmPassword')}
           ></Form.Control>
+          <small>{formik.errors.confirmPassword}</small>
         </Form.Group>
 
-        <Button type="submit" variant="primary" className="mt-3">
+        <Button disabled={!formik.isValid} type="submit" variant="primary" className="mt-3">
           Register
         </Button>
       </Form>
