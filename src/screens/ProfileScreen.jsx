d2b26e28190e18496e640b67ff51adf0943780ab
itx-shoaib <shoaibjamil43@gmail.com>
2023-07-25 01:33:24 +0500

    Adding formik in profile screen

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,5 +1,3 @@
-import { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
@@ -7,43 +5,51 @@ import { toast } from 'react-toastify';
 import Loader from '../components/Loader';
 import { useUpdateUserMutation } from '../slices/userApiSlice';
 import { setCredentials } from '../slices/authSlice';
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  email: yup.string()
+  .email("Provide a valid email address")
+  .required("Email is required"),
+name: yup.string().required("Name is required"),
+});
 
 const ProfileScreen = () => {
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-
   const { userInfo } = useSelector((state) => state.auth);
 
   const dispatch = useDispatch();
 
 const [updateProfile, { isLoading }] = useUpdateUserMutation();
 
-useEffect(() => {
-  setName(userInfo.name);
-  setEmail(userInfo.email);
-}, [userInfo.email, userInfo.name]);
 
-const submitHandler = async (e) => {
-    e.preventDefault();
-    if (password !== confirmPassword) {
-      toast.error('Passwords do not match');
-    } else {
-      try {
-        const res = await updateProfile({
-          _id: userInfo._id,
-          name,
-          email,
-          password,
-        }).unwrap();
-        dispatch(setCredentials({ ...res }));
-        toast.success('Profile updated successfully');
-      } catch (err) {
-        toast.error(err?.data?.message || err.error);
+  const formik = useFormik({
+    initialValues: {
+      email: userInfo.email,
+      name: userInfo.name,
+      password: '',
+      confirmPassword: ''
+    },
+    validationSchema: schema,
+    onSubmit: async (values) => {
+      if (values.password !== values.confirmPassword) {
+        toast.error('Passwords do not match');
+      } else {
+        try {
+          const res = await updateProfile({
+            _id: userInfo._id,
+            name:values.name,
+            email : values.email,
+            password : values.password,
+          }).unwrap();
+          dispatch(setCredentials({ ...res }));
+          toast.success('Profile updated successfully');
+        } catch (err) {
+          toast.error(err?.data?.message || err.error);
+        }
       }
-    }
-  };
+    },
+  });
 
   if(isLoading) return <Loader/>
 
@@ -51,33 +57,38 @@ const submitHandler = async (e) => {
     <FormContainer>
       <h1>Update Profile</h1>
 
-      <Form onSubmit={submitHandler}>
+      <Form onSubmit={formik.handleSubmit}>
         <Form.Group className='my-2' controlId='name'>
           <Form.Label>Name</Form.Label>
           <Form.Control
             type='name'
             placeholder='Enter name'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={formik.values.name}
+            {...formik.getFieldProps('name')}
           ></Form.Control>
+          <small>{formik.errors.name}</small>
         </Form.Group>
+
         <Form.Group className='my-2' controlId='email'>
           <Form.Label>Email Address</Form.Label>
           <Form.Control
             type='email'
             placeholder='Enter email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={formik.values.email}
+            {...formik.getFieldProps('email')}
           ></Form.Control>
+          <small>{formik.errors.email}</small>
         </Form.Group>
+
         <Form.Group className='my-2' controlId='password'>
           <Form.Label>Password</Form.Label>
           <Form.Control
             type='password'
             placeholder='Enter password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={formik.values.password}
+            {...formik.getFieldProps('password')}
           ></Form.Control>
+          <small>{formik.errors.password}</small>
         </Form.Group>
 
         <Form.Group className='my-2' controlId='confirmPassword'>
@@ -85,12 +96,13 @@ const submitHandler = async (e) => {
           <Form.Control
             type='password'
             placeholder='Confirm password'
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
+            value={formik.values.confirmPassword}
+            {...formik.getFieldProps('confirmPassword')}
           ></Form.Control>
+          <small>{formik.errors.confirmPassword}</small>
         </Form.Group>
 
-        <Button type='submit' variant='primary' className='mt-3'>
+        <Button disabled={!formik.isValid} type='submit' variant='primary' className='mt-3'>
           Update
         </Button>
       </Form>
