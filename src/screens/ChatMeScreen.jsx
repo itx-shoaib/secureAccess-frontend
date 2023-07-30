886f4e71670858917ca32bc83162626815b1fc0a
itx-shoaib <shoaibjamil43@gmail.com>
2023-07-30 23:30:08 +0500

    Adding scrollbar to end

--- src/screens/ChatMeScreen.jsx
+++ src/screens/ChatMeScreen.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import MessageBody from '../components/ChatMe/MessageBody'
 import MessageSend from '../components/ChatMe/MessageSend'
+import ScrollableFeed from 'react-scrollable-feed'
 
 const ChatMeScreen = () => {
   return (
-    <div>
+    <ScrollableFeed>
       <MessageBody/>
       <MessageSend/>
-    </div>
+    </ScrollableFeed>
   )
 }
 
