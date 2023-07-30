37f986fd065bf39d433d9a97c8c0b0ba62a71594
itx-shoaib <shoaibjamil43@gmail.com>
2023-07-30 23:25:25 +0500

    Making the message Body

--- src/components/ChatMe/MessageBody.jsx
+++ src/components/ChatMe/MessageBody.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import { message } from './utils';
 
 const MessageBody = () => {
   return (
-    <div style={{ height: "80vh", background: "red", display: "flex", flexDirection: "column" }}>
-      <div style={{ maxWidth: "50%" }}>
-        <Card>
-          <Card.Body>This is some text within a card body.</Card.Body>
+    <div style={{ height: "80vh",width:"100%", background: "red", display: "flex", flexDirection: "column", overflow:"scroll",  }}>
+    {message.map((messages)=> (
+      <div key={messages.id} style={{ padding:"10px",display: "flex", justifyContent: messages.sendBy === "user" && "flex-end" }}>
+      <div style={{maxWidth: "50%",display: "flex",flexDirection:"column"}}>
+        <Card >
+          <Card.Body>{messages.message}</Card.Body>
         </Card>
+        <p>12:12 am</p>
       </div>
-      <div style={{ display: "flex", justifyContent: "flex-end" }}>
-        <h1>Message Body 2</h1>
       </div>
-      <h1>Message Body 3</h1>
+      )
+    )}
     </div>
   );
 }
---
+++ src/components/ChatMe/utils.js
@@ -0,0 +1,8 @@
+export const message = [
+    {id:1,message:"Hi!",sendBy:"chatbot"},
+    {id:2,message:"Hy How are you?",sendBy:"user"},
+    {id:3,message:"I am good whats about you?",sendBy:"chatbot"},
+    {id:4,message:"I am also good",sendBy:"user"},
+    {id:5,message:"Whats going on?",sendBy:"chatbot"},
+    {id:6,message:"Nothing just chiling",sendBy:"user"},
+  ]
